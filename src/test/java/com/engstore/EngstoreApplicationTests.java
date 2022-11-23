a59539d43e109fe0695a0de1c7e0e0df01806d37
epike <epikeran@gmail.com>
2022-11-23 15:17:42 +0900

    Springboot Setting

---
+++ src/test/java/com/engstore/EngstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
